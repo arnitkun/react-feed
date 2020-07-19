a56a33ca4c53c206b5929e085620dd3abcd9b5ad
arnitkun <ankit.nitagartala@gmail.com>
2020-07-20 02:20:43 +0530

    added div

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,12 +8,13 @@ import '../index.css';
 class Navbar extends Component {
     render(){
         return (
+            <div className = "navbar">
                 <Menu mode="horizontal"  defaultSelectedKeys={['2']}>
                 <Menu.Item key="1">nav 1</Menu.Item>
                 <Menu.Item key="2">nav 2</Menu.Item>
                 <Menu.Item key="3">nav 3</Menu.Item>
                 </Menu>
-            
+                </div>
         )
     }
 }
