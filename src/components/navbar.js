262a48606d8fe6a09b38ff972b401cb7b1db4e19
arnitkun <ankit.nitagartala@gmail.com>
2020-07-19 06:21:02 +0530

    basic ant design navbar test

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Menu} from 'antd'
+
+
+import 'antd/dist/antd.css';
+import '../index.css';
+
+class Navbar extends Component {
+    render(){
+        return (
+                <Menu mode="horizontal"  defaultSelectedKeys={['2']}>
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            
+        )
+    }
+}
+
+export default Navbar;
