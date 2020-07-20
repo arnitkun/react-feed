60d0b5046403a59b1456a3c4e20535311dcd26e9
arnitkun <ankit.nitagartala@gmail.com>
2020-07-20 23:36:25 +0530

    getting news from the api call to feed components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,17 +7,39 @@ import NewsCardRow from './components/cardRow'
 // import List from './components/list'
 import Inflist from './components/infscroller'
 
-function App() {
-  return (
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+  componentDidMount(){
+    this.getNews();
+  }
+
+  getNews = () => {
+    var topurl = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=8bcdc13d04f144d38b2e837242ebff7d';
+   
+    let data = fetch(topurl);
+    data.then(res => {
+      return res.json();
+    }).then(n => {
+      this.setState({news:n})
+    })
+    
+  }
+  render(){
+    if(!this.state.news) {
+      return(<h4>Loading...</h4>)
+    } else {
+    return (
     <div className="App">
-      
       <Navbar/>
-      {/* <NewsCard /> 
-      <NewsCard />
-      <NewsCard /> */}
-      <Inflist />
-    </div>
-  );
+        <Inflist data={this.state.news}/>
+      </div>
+    );
+  }
+}
 }
 
 export default App;
