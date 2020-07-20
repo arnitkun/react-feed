b97288e2e37ca906d3df70c3f21cebaa0230fbb8
arnitkun <ankit.nitagartala@gmail.com>
2020-07-20 23:36:43 +0530

    support for one card per row

--- src/components/cardRow.js
+++ src/components/cardRow.js
@@ -4,37 +4,38 @@ import {Card, Col, Row} from 'antd';
 import 'antd/dist/antd.css';
 import '../index.css';
 
+const {Meta} = Card;
 
-var s = "bing"
-var Headline = "some shit"
-var content = "more shit"
 class NewsCardRow extends Component{
 
     render() {
         return (
             <div className="site-card-wrapper">
-    <Row gutter={16}>
-      <Col span={8}>
-      <Card title="Card title" bordered={true}>
-                <div >Headline: {this.props.Headline1}
+    <Row gutter={8}>
+      <Col span={12}>
+      <Card 
+      hoverable
+      // cover={<img alt="image" src={}/>}
+      title={this.props.card_1_info.title} bordered={true}>
+                <div >Source: {this.props.card_1_info.source.name}
                 </div>
-                Card content
+                {<img class="card-image" alt="Loading..." src={this.props.card_1_info.urlToImage}/>}
             </Card>
       </Col>
-      <Col span={8}>
-        <Card title="Card title" bordered={true}>
-        <div >Headline: {this.props.Headline2}
-            </div>
-          Card content
-        </Card>
-      </Col>
-      <Col span={8}>
+       {/* <Col span={12}>
+         <Card title="Card title" bordered={true}>
+         <div >Headline: {this.props.Headline2}
+             </div>
+           Card content
+         </Card>
+       </Col> */}
+      {/* <Col span={8}>
         <Card title="Card title" bordered={true}>
         <div >Headline: {this.props.Headline3}
             </div>
           Card content
         </Card>
-      </Col>
+      </Col> */}
     </Row>
   </div>
         )
