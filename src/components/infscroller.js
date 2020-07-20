06a5c1e3407e5cc76c34e0c709ab237ab4aeb7a3
arnitkun <ankit.nitagartala@gmail.com>
2020-07-21 03:21:57 +0530

    component now receives news  as prop from parent

--- src/components/infscroller.js
+++ src/components/infscroller.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { Modal,Empty} from 'antd'
 
 import NewsCardRow from './cardRow'
 
@@ -11,43 +12,97 @@ const style = {
 };
 
 class Inflist extends React.Component {
-  state = {
-    items: Array.from({ length: 20 })
-  };
-
-  componentDidMount(){
-    this.setState(this.props)
+  constructor(props){
+    super(props)
+      this.state = {
+        detailsVisible:false,
+        news:this.props
+      };
+    
   }
-  fetchMoreData = () => {
-    // a fake async api call like which sends
-    // 20 more records in 1.5 secs
-    setTimeout(() => {
-      this.setState({
-        items: this.state.items.concat(Array.from({ length: 20 }))
-      });
-    }, 1500);
-    console.log(this.props)
-  };
 
-  render() {
-    return (
-      <div>
+  // componentDidMount(){
+  //   this.setState(this.props)
+  // }
 
+  handleView = () => {
+    if(this.state.detailsVisible === false){
+      return(
+        <div>
         <InfiniteScroll
-          dataLength={this.props.data.totalResults}
-          next={this.fetchMoreData}
-          hasMore={true}
+          dataLength={this.state.news.data.totalResults}
+          next={this.getNews}
+          hasMore={this.state.news.data.articles.length<this.state.news.data.totalResults}
           loader={<h4>Loading...</h4>}
+          endMessage={<h4>No more news. Please refresh or select a source from the navigation bar.</h4>}
         >
-          {this.props.data.articles.map((article, articlenumber) => (
+          {this.state.news.data.articles.map((article, articlenumber) => (
             // <div style={style} key={index}>
             //   div - #{index}
             // </div>
             // <NewsCardRow card_1_info = {index} Headline2 = {index+1} Headline3 = {index+2}/>
-            <NewsCardRow card_1_info = {article}/>
+            <NewsCardRow card_1_info = {article} onClick={this.handleViewChange}/>
           ))}
         </InfiniteScroll>
       </div>
+      )
+    } else{
+      return(
+        <div>
+            <Empty
+                  image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                  imageStyle={{
+                   height: 60,
+                  }}
+                  description={
+                    <span>
+                     Details page
+                      </span>
+                  }
+  >
+    
+  </Empty>,
+        </div>
+      )
+    }
+  }
+
+  handleViewChange = () => {
+    if(this.state.detailsVisible == false){
+      this.state.detailsVisible = true
+    } else {
+      this.state.detailsVisible = false
+    }
+  }
+
+  // fetchMoreData = () => {
+  //   setTimeout(() => {
+  //     this.setState({
+  //       items: this.state.items.concat(Array.from({ length: 20 }))
+  //     });
+  //   }, 1500);
+  // };
+
+  getNews = () => {
+      var allurl = 'https://newsapi.org/v2/everything?sources=engadget&apiKey=8bcdc13d04f144d38b2e837242ebff7d'
+     
+      let data = fetch(allurl);
+      data.then(res => {
+        return res.json();
+      }).then(n => {
+
+        let newNews = Object.assign({},this.state.news,  n)
+
+        this.setState({news:newNews})
+      })
+      
+  };
+
+  
+
+  render() {
+    return (
+      this.handleView()
     );
   }
 }
