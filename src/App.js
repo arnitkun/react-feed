f45b45771d15f32e682b69114e763dae4a558035
arnitkun <ankit.nitagartala@gmail.com>
2020-07-22 00:35:40 +0530

    initial source selection from  nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 import Navbar from './components/navbar'
-import NewsCardRow from './components/cardRow'
-// import List from './components/list'
 import Inflist from './components/infscroller'
 
 class App extends Component{
@@ -17,8 +15,24 @@ class App extends Component{
     this.getNews();
   }
 
+  handleSource = (source="engadget") => {
+    console.log("source changed! Source = " + source);
+    this.parseUrl(source)
+  }
+
+  parseUrl = (param) =>{
+
+    let url = 'https://newsapi.org/v2/top-headlines?country=in&pagesize=40&apiKey=8bcdc13d04f144d38b2e837242ebff7d' 
+    if(param != ""){
+      url = 'https://newsapi.org/v2/everything?sources='+param+'&pagesize=40&apiKey=8bcdc13d04f144d38b2e837242ebff7d'
+    }
+    console.log("url parsed! New url = " + url)
+    this.updateNews(url)
+  }
+
   getNews = () => {
-    var topurl = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=8bcdc13d04f144d38b2e837242ebff7d';
+    console.log("Getting top news")
+    var topurl = 'https://newsapi.org/v2/top-headlines?country=in&pagesize=40&apiKey=8bcdc13d04f144d38b2e837242ebff7d'
    
     let data = fetch(topurl);
     data.then(res => {
@@ -28,13 +42,23 @@ class App extends Component{
     })
     
   }
+
+  updateNews = (url) => {
+    let data = fetch(url);
+    data.then(res => {
+      return res.json();
+    }).then(n => {
+      this.setState({news:n})
+    })
+  }
+  
   render(){
     if(!this.state.news) {
       return(<h4>Loading...</h4>)
     } else {
     return (
     <div className="App">
-      <Navbar/>
+      <Navbar onSourceChange = {this.handleSource}/>
         <Inflist data={this.state.news}/>
       </div>
     );
