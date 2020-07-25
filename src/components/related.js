a8715d36cf992cd11174eabc342cb58f818f03b7
arnitkun <ankit.nitagartala@gmail.com>
2020-07-25 22:10:16 +0530

    initial commit for component

---
+++ src/components/related.js
@@ -0,0 +1,19 @@
+import React, { Component} from 'react';
+import {Card, Col, Row} from 'antd';
+
+import 'antd/dist/antd.css';
+import '../index.css';
+
+const {Meta} = Card;
+
+class related extends Component{
+  constructor(props){
+    super(props)
+  }
+  
+    render(){
+        return(<div>related</div>)
+    }
+  }
+
+  export default related;
